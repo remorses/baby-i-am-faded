01a157a8ba7c2d36e07f4a3555286c6e304ee1ce
remorses <beats.by.morse@gmail.com>
2021-11-05 22:47:28 +0100

    rm stuff

--- website/next.config.js
+++
@@ -1,8 +0,0 @@
-const compose = require('compose-function')
-const withTM = require('next-transpile-modules')(['baby-i-am-faded'])
-
-const composed = compose(withTM,)
-
-module.exports = composed({
-    pageExtensions: ['js', 'jsx', 'mdx', 'tsx'],
-})
