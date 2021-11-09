e6a1a39da9c4e00a8c81b75c45d8d1439e1a0bdc
remorses <beats.by.morse@gmail.com>
2021-11-09 14:22:04 +0100

    fix reduced motion for cascade

--- baby-i-am-faded/scripts/generate-styles.ts
+++ baby-i-am-faded/scripts/generate-styles.ts
@@ -16,9 +16,31 @@ function main() {
         generatedCss += `    --delay: calc(var(--initial-delay, 0ms) + ${index} * var(--increment));\n`
         generatedCss += `}\n`
     }
+    generatedCss += '\n/* This has been generated too */\n'
+    generatedCss += `@media (max-width: 500px) ` + disableStyle
+    generatedCss += `@media (prefers-reduced-motion) ` + disableStyle
+
     const newStyles =
         styles.slice(0, styles.indexOf(commentForReplacement)) + generatedCss
+
     fs.writeFileSync(stylesPath, newStyles)
 }
+
+const disableStyle = `{
+    .biafNonCascade {
+        animation-name: none;
+    }
+    .biafTextWord {
+        animation-name: none;
+    }
+    .biafCascade > :not(style):nth-child(-n + 30) {
+        animation-name: none !important;
+    }
+    .biafHidden {
+        opacity: unset;
+    }
+}
+
+`
 
 main()
